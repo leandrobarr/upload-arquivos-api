c33544f1ca26a27382e8c860e5cda62d8de2b3d7
leandrobarr <leandrobarr@yahoo.com.br>
2022-05-18 10:26:26 -0300

    first commit

---
+++ src/main/java/br/com/lgm/uploadarquivosapi/controller/UploadArquivoController.java
@@ -0,0 +1,4 @@
+package br.com.lgm.uploadarquivosapi.controller;
+
+public class UploadArquivoController {
+}
---
+++ src/test/java/br/com/lgm/uploadarquivosapi/UploadArquivosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lgm.uploadarquivosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadArquivosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
